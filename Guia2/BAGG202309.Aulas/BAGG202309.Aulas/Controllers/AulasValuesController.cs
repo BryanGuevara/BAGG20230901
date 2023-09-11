50e8ee3f8a881cd7e03481fac34881e57d679cbf
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2023-09-11 15:01:25 -0600

    Guia 2 - Ejercicio #4

---
+++ Guia2/BAGG202309.Aulas/BAGG202309.Aulas/Controllers/AulasValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BAGG202309.Aulas.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BAGG202309.Aulas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AulasValuesController : ControllerBase
+    {
+        static List<Aula> aulas = new List<Aula>
+    {
+        new Aula { Id = 1, Salon = "1° A", Maestro = "Rosy" },
+        new Aula { Id = 2, Salon = "2° B", Maestro = "Cristina" },
+        new Aula { Id = 3, Salon = "3° C", Maestro = "Marvin" }
+    };
+
+        [HttpGet]
+        public IEnumerable<Aula> Get()
+        {
+            return aulas;
+        }
+
+    }
+}
---
+++ Guia2/BAGG202309.Aulas/BAGG202309.Aulas/Models/Aula.cs
@@ -0,0 +1,9 @@
+﻿namespace BAGG202309.Aulas.Models
+{
+    public class Aula
+    {
+        public int Id { get; set; }
+        public string Salon { get; set; }
+        public string Maestro { get; set; }
+    }
+}
